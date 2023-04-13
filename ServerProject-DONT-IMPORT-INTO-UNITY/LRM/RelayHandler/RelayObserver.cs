7142e4199bb2a157867e5268b42225426b8f0722
NBK <1justforstuding1@gmail.com>
2023-04-13 15:05:10 +0300

    Фикс ошибки с заголовками, и повторной авторизации

--- ServerProject-DONT-IMPORT-INTO-UNITY/LRM/RelayHandler/RelayObserver.cs
+++ ServerProject-DONT-IMPORT-INTO-UNITY/LRM/RelayHandler/RelayObserver.cs
@@ -13,8 +13,6 @@ namespace LightReflectiveMirror
         private const string BEARER_FILE_NAME = "bearer.json";
         private const string LOGIN_FILE_NAME = "login.json";
         
-        private const int MAX_NUMBER_ATTEMPT = 5;
-
         private API _api;
         private Login _login;
         private Bearer _bearer;
@@ -31,8 +29,9 @@ namespace LightReflectiveMirror
 
         public async Task Authentication()
         {
-            _api ??= await FileWorker.ReadFile<API>(SETTINGS_FILE_NAME);
-            _login ??= await FileWorker.ReadFile<Login>(LOGIN_FILE_NAME);
+            _api = await FileWorker.ReadFile<API>(SETTINGS_FILE_NAME);
+            _login = await FileWorker.ReadFile<Login>(LOGIN_FILE_NAME);
+            _bearer = null;
 
             using HttpRequestMessage request = new HttpRequestMessage(
                 HttpMethod.Post, 
@@ -43,8 +42,10 @@ namespace LightReflectiveMirror
             {
                 foreach (var headerValue in header.Value)
                 {
-                    if (string.Equals(header.Key , "Authorization"))
-                        FileWorker.WriteInFile(new Bearer(header.Key, headerValue), BEARER_FILE_NAME);
+                    if (string.Equals(header.Key, "Authorization"))
+                    {
+                        await FileWorker.WriteInFile(new Bearer(header.Key, headerValue), BEARER_FILE_NAME);
+                    }
                 }
             }
         }
@@ -58,14 +59,14 @@ namespace LightReflectiveMirror
                 HttpMethod.Post,
                 _api.Domain + string.Format(_api.ClearAccessTokenProviderPath, accessToken));
             
-            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
-            httpClient.DefaultRequestHeaders.Add(_bearer.Name, "Bearer " + _bearer.Token);
+            request.Headers.Add("Accept", "application/json");
+            request.Headers.Add(_bearer.Name, "Bearer " + _bearer.Token);
             
             using HttpResponseMessage response = await httpClient.SendAsync(request);
-
+            Program.WriteLogMessage($"Status Code = {response.StatusCode}", ConsoleColor.Yellow);
             if (response.StatusCode == HttpStatusCode.Unauthorized)
             {
-                if (attempt == MAX_NUMBER_ATTEMPT)
+                if (attempt == _api.MaxNumberAttemptReConnect)
                     Program.WriteLogMessage($"{DateTime.Now}; Exception: attempts are over;", ConsoleColor.Red);
                 await Authentication();
                 Clear(accessToken, ++attempt);
